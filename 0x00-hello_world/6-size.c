0c23b99f2d9a633291e2dc838793510893f8001f
Gicheha-jr <lastb702@gmail.com>
2023-02-16 11:57:46 -0800

    mourn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
