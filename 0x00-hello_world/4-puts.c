23d74f55937e3413888de66fa746396602026b2c
Gicheha-jr <lastb702@gmail.com>
2023-02-16 10:10:20 -0800

    iknow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A programe that print a line with puts function 
  * Return 0 (Success)
  */
-int main (void)
+int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
 return (0);
