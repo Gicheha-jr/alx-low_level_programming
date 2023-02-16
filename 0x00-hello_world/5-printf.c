b137295a14af6e9e175067895d072f043bf8c08c
Gicheha-jr <lastb702@gmail.com>
2023-02-16 10:53:17 -0800

    mime

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with printf function
+ * retern 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
