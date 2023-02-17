c090cf9782cf1a5a6705851200e60b45be9f2d36
Gicheha-jr <lastb702@gmail.com>
2023-02-17 11:37:46 -0800

    another

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+
+		}
+	putchar('/n');
+	return (0);
+}
