809733024b5758835065dae9f2e8173512551d2b
Gicheha-jr <lastb702@gmail.com>
2023-02-23 08:37:17 -0800

    join

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -8,11 +8,11 @@
 void print_numbers(void)
 
 {
-	int x;
+        int x;
 
-	for (x = 0; x <= 9; x++)
-	{
-	_putchar(x + '0');
-	}
-	_putchar('\n');
+        for (x = 0; x <= 9; x++)
+        {
+        _putchar(x + '0');
+        }
+        _putchar('\n');
 }
