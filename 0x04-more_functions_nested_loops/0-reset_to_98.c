73c1eab1cbd40388a65ce4e71d4df5182e0f267e
Gicheha-jr <lastb702@gmail.com>
2023-02-27 05:25:28 -0800

    first

---
+++ 0x04-more_functions_nested_loops/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer to an int and update
+ * the value it points to 98
+ *
+ * @n: An integer input pointer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
